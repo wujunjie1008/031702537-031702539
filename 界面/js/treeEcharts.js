0cfe41b7751b68bda746d043d08f06c0d1daf0da
LiQingYu <1781610742@qq.com>
2019-10-17 21:16:31 +0800

    1.5修改了输入格式

--- 界面/js/treeEcharts.js
+++ 界面/js/treeEcharts.js
@@ -1,10 +1,10 @@
 
 var data = [
-		{name:"广东",value:"01",sj:"-"},
-		{name:"广州",value:"0101",sj:"01"},
-		{name:"潮州",value:"0102",sj:"01"},
-		{name:"深圳",value:"0103",sj:"01"},
-		{name:"茂名",value:"0104",sj:"01"},
+		//{name:"导师：张三",value:"01",sj:"-"},
+		//{name:"本科生",value:"0101",sj:"01"},
+		//{name:"硕士生",value:"0102",sj:"01"},
+		//{name:"博士生",value:"0103",sj:"01"},
+		/*{name:"茂名",value:"0104",sj:"01"},
 		{name:"揭阳",value:"0105",sj:"01"},
 		{name:"萝岗",value:"010101",sj:"0101"},
 		{name:"天河",value:"010102",sj:"0101"},
@@ -13,21 +13,62 @@ var data = [
 		{name:"花都",value:"010105",sj:"0101"},
 		{name:"海珠",value:"010106",sj:"0101"},
 		{name:"枫溪",value:"010201",sj:"0102"},
-		{name:"枫桥",value:"010202",sj:"0102"},
-		{name:"罗湖",value:"010301",sj:"0103"}
+		{name:"枫桥",value:"010202",sj:"0102"}*/
+		
 	];
 var treeData;
 var id = 1;
 
+function deal_form(form){
+	
+	var s = form.split(/[\n]/);
+	//alert(s[0]);
+	data.push({name:s[0],value:"01",sj:"-"});
+	//alert(s.length);/**/
+	var gValue = "00";
+	
+	for(var i=1;i<s.length;i++){		
+		var grade = s[i].split("：");
+		//alert(grade[0]);
+		
+		gValue++;
+		gValue.toString();
+		gValue = '0' + gValue;
+		var sj = "01";
+		//alert(grade[0]+gValue+sj);
+		data.push({name:grade[0],value:'01'+gValue,sj:sj});
+		
+		var name = grade[1].split("、");
+		//alert(name.length);
+		var nValue = "00";
+		for(var j=0; j<name.length; j++){
+			
+			nValue++;
+			nValue.toString();
+			nValue = '0' + nValue;
+			//alert(name[j]+gValue+nValue);
+			data.push({name:name[j],value:'01'+gValue+nValue,sj:'01'+gValue})
+		}
+	}
+	//alert(s.length);
+	
+
+}
+
+
+
 function draw(){
+	var searchform = document.getElementById("searchForm");
+	var form = searchform.Form.value;
+	//alert(form);
+	//处理表格
+	deal_form(form);
 	//2.处理数据
+	
 	if(document.getElementById('Form').value != "")
 	{treeData = transData(data, 'value', 'sj', 'children');}
 	//3.展示树
 	drawTree(treeData,id);
-	var searchform = document.getElementById("searchForm");
-	var form = searchform.Form.value;
-	alert(form);
 	document.getElementById('Form').value = "";
 	id = id + 1;
 }
